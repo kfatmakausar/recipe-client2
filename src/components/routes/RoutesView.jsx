2ef3695b15bdd27fdc67901ecb2e3d85349d335d
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-20 00:23:19 -0400

    Added the the containers, routes, views, and reducers to components

---
+++ src/components/routes/RoutesView.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Login, Signup, AllBooksContainer } from "../containers";
+
+const RoutesView = (props) => {
+  const { isLoggedIn } = props;
+
+  return (
+    <Switch>
+      {/* Routes placed within this section are available to all visitors */}
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+
+      {isLoggedIn && (
+        <Switch>
+          {/* Routes placed within this section are only available after
+          logging in */}
+          <Route exact path="/books" component={AllBooksContainer} />
+        </Switch>
+      )}
+
+      {/* Displays our Login component as a fallback */}
+      <Route component={Login} />
+    </Switch>
+  );
+};
+
+export default RoutesView;
